8ca47526a6c06047302b89460821717083b56d3e
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-03-22 14:12:37 -0600

    latest version of yarn and npm

--- tools/node/install.sh
+++ tools/node/install.sh
@@ -3,8 +3,6 @@ set -e
 set -o pipefail
 
 NODE_VERSION="${1:?Missing node version as first argument}"
-NPM_VERSION="${2:?Missing npm version as second argument}"
-YARN_VERSION="${3:?Missing yarn version as third argument}"
 
 ARCH=x64
 NODE_TAR_URL="https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-${ARCH}.tar.xz"
@@ -13,6 +11,6 @@ NODE_TAR_URL="https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-lin
   curl -fSsL "$NODE_TAR_URL" | tar -xJf - --strip-components=1
   ldconfig
   node --version
-  npm install -g "npm@$NPM_VERSION" "yarn@$YARN_VERSION"
+  npm install -g npm yarn
   npm cache clean --force
 )
