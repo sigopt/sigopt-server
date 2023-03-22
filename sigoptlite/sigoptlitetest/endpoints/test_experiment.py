3fd07d71e256d725484e5c788285ff4593a61022
Eric Hans Lee <erichanslee@gmail.com>
2023-03-22 11:26:53 -0700

    more tests

--- sigoptlite/sigoptlitetest/endpoints/test_experiment.py
+++ sigoptlite/sigoptlitetest/endpoints/test_experiment.py
@@ -1,9 +1,14 @@
 # Copyright © 2022 Intel Corporation
 #
 # SPDX-License-Identifier: Apache License 2.0
+
 import pytest
 
+from sigopt import Connection
+
+from sigoptlite.driver import LocalDriver
 from sigoptlitetest.endpoints.base_test import UnitTestsEndpoint
+from sigoptlite.models import FIXED_EXPERIMENT_ID
 
 
 class TestExperimentBasic(UnitTestsEndpoint):
@@ -20,28 +25,50 @@ class TestExperimentBasic(UnitTestsEndpoint):
     experiment = self.conn.experiments().create(**experiment_meta)
     assert experiment.observation_budget is None
 
+  # TODO: need a much more descriptive error when user tries to do this
+  def test_other_endpoint_before_experiment_create(self):
+    conn = Connection(driver=LocalDriver)
+    with pytest.raises(Exception):
+      conn.experiments(FIXED_EXPERIMENT_ID).fetch()
+
+    with pytest.raises(Exception):
+      conn.experiments(FIXED_EXPERIMENT_ID).suggestions().create()
+
+    with pytest.raises(Exception):
+      conn.experiments(FIXED_EXPERIMENT_ID).observations().create(suggestion=1, values={"name": "y", "value": 1})
+
+    with pytest.raises(Exception):
+      conn.experiments(FIXED_EXPERIMENT_ID).observations().fetch()
+
+    with pytest.raises(Exception):
+      conn.experiments(FIXED_EXPERIMENT_ID).observations().fetch()
+
+    with pytest.raises(Exception):
+      conn.experiments(FIXED_EXPERIMENT_ID).best_assignments().fetch()
+
 
 class TestExperimentParameters(UnitTestsEndpoint):
   @pytest.mark.xfail  # Not being handled property atm
   def test_no_parameters(self):
     experiment_meta = self.get_experiment_feature("default")
     del experiment_meta["parameters"]
-    with pytest.raises(AssertionError):
+    with pytest.raises(ValueError):
       self.conn.experiments().create(**experiment_meta)
 
+  @pytest.mark.xfail  # Not being handled, should be dealt with validation refactor
   def test_empty_parameters(self):
     experiment_meta = self.get_experiment_feature("default")
     experiment_meta["parameters"] = []
-    with pytest.raises(AssertionError):
+    with pytest.raises(ValueError):
       self.conn.experiments().create(**experiment_meta)
 
   @pytest.mark.xfail  # Not being handled, should be dealt with validation refactor
   def test_experiment_create_extra_parameters(self):
     experiment_meta = self.get_experiment_feature("default")
-    with pytest.raises(AssertionError):
+    with pytest.raises(ValueError):
       self.conn.experiments().create(extra_param=True, **experiment_meta)
 
-  @pytest.mark.xfail  # Not being handled, wait for Gustavo's merge
+  @pytest.mark.xfail  # Not being handled, should be dealt with validation refactor
   def test_experiment_create_wrong_parameter_types(self, any_meta):
     any_meta["parameters"][0]["type"] = "invalid_type"
     with pytest.raises(ValueError):
@@ -63,16 +90,63 @@ class TestExperimentParameters(UnitTestsEndpoint):
     assert cat2.enum_index == 2
     assert cat2.name == "e"
 
+  def test_create_with_correct_parameter_priors(self):
+    experiment_meta = self.get_experiment_feature("priors")
+    e = self.conn.experiments().create(**experiment_meta)
+    parameter_normal = e.parameters[1]
+    assert hasattr(parameter_normal, "prior")
+    assert parameter_normal.prior.mean == 5.0
+    assert parameter_normal.prior.scale == 2.0
+    assert parameter_normal.prior.shape_a is None
+    assert parameter_normal.prior.shape_b is None
+
+    parameter_beta = e.parameters[5]
+    assert hasattr(parameter_beta, "prior")
+    assert parameter_beta.prior.shape_a == 1
+    assert parameter_beta.prior.shape_b == 10
+    assert parameter_beta.prior.mean is None
+    assert parameter_beta.prior.scale is None
+
+  @pytest.mark.xfail  # Not being handled, should be dealt with validation refactor
+  @pytest.mark.parametrize("scale", [0, -1, -10])
+  def test_create_with_incorrect_normal_prior(self, scale):
+    parameter_normal = dict(
+      name="parameter_normal",
+      type="double",
+      bounds=dict(min=0, max=10),
+      prior=dict(name="normal", mean=5, scale=scale),
+    )
+    with pytest.raises(ValueError):
+      self.conn.experiments().create(
+        parameters=[parameter_normal],
+        metrics=[dict(name="y1", objective="maximize", strategy="optimize")],
+      )
+
+  @pytest.mark.xfail  # Not being handled, should be dealt with validation refactor
+  @pytest.mark.parametrize("shape_a, shape_b", [(1, -1), (-1, 1), (-1, -1), (0, 5), (5, 0), (0, 0)])
+  def test_create_with_incorrect_beta_prior(self, shape_a, shape_b):
+    parameter_beta = dict(
+      name="parameter_beta",
+      type="double",
+      bounds=dict(min=0, max=1),
+      prior=dict(name="beta", shape_a=-1, shape_b=10),
+    )
+    with pytest.raises(ValueError):
+      self.conn.experiments().create(
+        parameters=[parameter_beta],
+        metrics=[dict(name="y1", objective="maximize", strategy="optimize")],
+      )
+
 
 class TestExperimentMetrics(UnitTestsEndpoint):
-  @pytest.mark.xfail  # Not being handled, wait for Gustavo's merge
+  @pytest.mark.xfail  # Not being handled, should be dealt with validation refactor
   def test_no_metrics(self):
     experiment_meta = self.get_experiment_feature("default")
     del experiment_meta["metrics"]
     with pytest.raises(ValueError):
       self.conn.experiments().create(**experiment_meta)
 
-  @pytest.mark.xfail  # Not being handled, wait for Gustavo's merge
+  @pytest.mark.xfail  # Not being handled, should be dealt with validation refactor
   def test_empty_metrics(self):
     experiment_meta = self.get_experiment_feature("default")
     experiment_meta["metrics"] = []
