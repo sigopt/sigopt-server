18c3f75bc7197bed3308505b75e6e7a785861313
malkomes <74630871+malkomes@users.noreply.github.com>
2023-03-23 16:55:01 -0500

    metric check

--- sigoptlite/sigoptlite/builders.py
+++ sigoptlite/sigoptlite/builders.py
@@ -793,7 +793,8 @@ class MetricEvaluationBuilder(BuilderBase):
     assert isinstance(input_dict["name"], str)
     assert isinstance(input_dict["value"], (int, float))
     if "value_stddev" in input_dict:
-      assert isinstance(input_dict["value"], (int, float))
+      assert isinstance(input_dict["value_stddev"], (int, float))
+      assert input_dict["value_stddev"] > 0
 
   @classmethod
   def create_object(cls, **input_dict):
