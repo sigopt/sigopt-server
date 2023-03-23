3b66cf7abb9c925741a0a81f315d298fd5bddee6
Eric Hans Lee <erichanslee@gmail.com>
2023-03-22 17:26:03 -0700

    disclaimer

--- sigoptlite/sigoptlitetest/features/test_conditionals.py
+++ sigoptlite/sigoptlitetest/features/test_conditionals.py
@@ -1,8 +1,11 @@
+# Copyright © 2022 Intel Corporation
+#
+# SPDX-License-Identifier: Apache License 2.0
 import pytest
-
-from sigoptlitetest.base_test import UnitTestsBase
-from sigoptlite.driver import LocalDriver
 from sigopt import Connection
+
+from sigoptlite.driver import LocalDriver
+from sigoptlitetest.base_test import UnitTestsBase
 
 
 class TestExperimentConditionals(UnitTestsBase):
