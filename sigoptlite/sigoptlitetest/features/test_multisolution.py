583f57b13eb51cd803444400fe7494720158c73d
Harvey <harvey.cheng@intel.com>
2023-03-27 14:02:55 -0700

    SigOptException

--- sigoptlite/sigoptlitetest/features/test_multisolution.py
+++ sigoptlite/sigoptlitetest/features/test_multisolution.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache License 2.0
 import pytest
 from sigopt import Connection
+from sigopt.exception import SigOptException
 
 from sigoptlite.driver import LocalDriver
 from sigoptlitetest.base_test import UnitTestsBase
@@ -15,7 +16,7 @@ class TestMultisolutionExperiment(UnitTestsBase):
   def test_multisolution_wrong_num_solutions(self, bad_num_sol):
     experiment_meta = self.get_experiment_feature("default")
     experiment_meta["num_solutions"] = bad_num_sol
-    with pytest.raises(ValueError) as exception_info:
+    with pytest.raises(SigOptException) as exception_info:
       self.conn.experiments().create(**experiment_meta)
     msg = ".num_solutions must be greater than or equal to 1"
     assert exception_info.value.args[0] == msg
@@ -23,7 +24,7 @@ class TestMultisolutionExperiment(UnitTestsBase):
   def test_multisolution_too_many_num_solutions(self):
     experiment_meta = self.get_experiment_feature("multisolution")
     experiment_meta["num_solutions"] = experiment_meta["observation_budget"] + 1
-    with pytest.raises(ValueError) as exception_info:
+    with pytest.raises(SigOptException) as exception_info:
       self.conn.experiments().create(**experiment_meta)
     msg = "observation_budget needs to be larger than the number of solutions"
     assert exception_info.value.args[0] == msg
@@ -31,7 +32,7 @@ class TestMultisolutionExperiment(UnitTestsBase):
   def test_multisolution_multitask_incompatible(self):
     experiment_meta = self.get_experiment_feature("multitask")
     experiment_meta["num_solutions"] = 5
-    with pytest.raises(ValueError) as exception_info:
+    with pytest.raises(SigOptException) as exception_info:
       self.conn.experiments().create(**experiment_meta)
     msg = "sigoptlite experiment with multiple solutions cannot be multitask"
     assert exception_info.value.args[0] == msg
@@ -39,7 +40,7 @@ class TestMultisolutionExperiment(UnitTestsBase):
   def test_multisolution_no_budget_forbidden(self):
     experiment_meta = self.get_experiment_feature("multisolution")
     experiment_meta.pop("observation_budget")
-    with pytest.raises(ValueError) as exception_info:
+    with pytest.raises(SigOptException) as exception_info:
       self.conn.experiments().create(**experiment_meta)
     msg = "observation_budget is required for a sigoptlite experiment with multiple solutions"
     assert exception_info.value.args[0] == msg
--- sigoptlite/sigoptlitetest/features/test_search.py
+++ sigoptlite/sigoptlitetest/features/test_search.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache License 2.0
 import pytest
 from sigopt import Connection
+from sigopt.exception import SigOptException
 
 from sigoptlite.driver import LocalDriver
 from sigoptlitetest.base_test import UnitTestsBase
@@ -16,7 +17,7 @@ class TestSearchExperiment(UnitTestsBase):
   def test_search_no_budget_forbidden(self, conn):
     experiment_meta = self.get_experiment_feature("search")
     experiment_meta.pop("observation_budget")
-    with pytest.raises(ValueError) as exception_info:
+    with pytest.raises(SigOptException) as exception_info:
       conn.experiments().create(**experiment_meta)
     msg = "observation_budget is required for a sigoptlite experiment with constraint metrics"
     assert exception_info.value.args[0] == msg
@@ -24,7 +25,7 @@ class TestSearchExperiment(UnitTestsBase):
   def test_search_multisolution_incompatible(self, conn):
     experiment_meta = self.get_experiment_feature("search")
     experiment_meta["num_solutions"] = 5
-    with pytest.raises(ValueError) as exception_info:
+    with pytest.raises(SigOptException) as exception_info:
       conn.experiments().create(**experiment_meta)
     msg = "sigoptlite experiment with multiple solutions require exactly one optimized metric"
     assert exception_info.value.args[0] == msg
