de4c356e8c1fd5dfac8e67d402b6bd3cd69696ef
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-03-28 14:38:02 -0600

    fix build for trivy scan

--- ci/trivy_scan_images.sh
+++ ci/trivy_scan_images.sh
@@ -6,10 +6,11 @@ source .env
 docker build . --tag=sigopt/nginx:scan --file=docker/images/nginx/Dockerfile \
   --build-arg=NGINX_VERSION="$NGINX_VERSION"
 docker build . --tag=sigopt/web:scan --file=docker/images/web/Dockerfile \
-  --build-arg=NODE_VERSION="$NODE_VERSION"
+  --build-arg=NODE_MAJOR="$NODE_MAJOR"
 docker build . --tag=sigopt/zigopt:scan --file=docker/images/zigopt/Dockerfile \
   --build-arg=PROTOBUF_VERSION="$PROTOBUF_VERSION" \
-  --build-arg=PYTHON_VERSION="$PYTHON_VERSION"
+  --build-arg=PYTHON_MAJOR="$PYTHON_MAJOR" \
+  --build-arg=PYTHON_MINOR="$PYTHON_MINOR"
 
 mkdir -p artifacts/trivy
 python ./docker/scan_application_images.py --registry=sigopt --tag=scan -- "$@"
