8c42c41bd4fa665054b6fa4569836644d065c9fc
Christopher Manteuffel <christopher.manteuffel@intel.com>
2023-03-28 18:41:26 -0700

    Move handler to prodapp

--- src/python/zigopt/api/prod.py
+++ src/python/zigopt/api/prod.py
@@ -7,8 +7,11 @@ from zigopt.api.request import Request
 from zigopt.api.routes import initialize_blueprint as initialize_base_blueprint
 from zigopt.api.routes import log_requests
 from zigopt.api.v1.routes import initialize_blueprint as initialize_v1_blueprint
+from zigopt.net.errors import BadParamError
 from zigopt.net.responses import success_response
 from zigopt.services.api import ApiRequestLocalServiceBag, ApiServiceBag
+
+from sigoptaux.errors import ValidationError
 
 
 class ProdApp(Flask):
@@ -23,6 +26,11 @@ class ProdApp(Flask):
     self.config_broker = config_broker
 
     config_broker.log_configs()
+
+    def handle_validation_errors(e):
+      return BadParamError(e.msg).get_error_response()
+
+    self.register_error_handler(ValidationError, handle_validation_errors)
 
     self.global_services = self.ServiceBag(config_broker, is_qworker=False)
     self.request_local_services_factory = self.RequestLocalServiceBag
