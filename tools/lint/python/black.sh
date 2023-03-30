57ffe4df5322d3c0f4f39dd49a2d53ac65cdf9bf
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-03-30 15:57:30 -0600

    black pre-commit

--- tools/lint/python/black.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Copyright © 2023 Intel Corporation
-#
-# SPDX-License-Identifier: Apache License 2.0
-set -e
-set -o pipefail
-
-black --config=tools/lint/python/pyproject.toml --experimental-string-processing "$@"
