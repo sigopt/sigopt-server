659955efb65f4678e056ed6c0aac01a988319617
Taylor Jackle Spriggs <74561858+tjs-intel@users.noreply.github.com>
2023-03-30 14:08:13 -0600

    Add license disclaimers to shell scripts (#76)
    * update disclaimer code
    
    * add disclaimers to shell scripts

--- scripts/launch/all_live.sh
+++ scripts/launch/all_live.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# Copyright © 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache License 2.0
+# Copyright © 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache License 2.0
 set -e
 set -o pipefail
 
--- scripts/launch/web_live.sh
+++ scripts/launch/web_live.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Copyright © 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache License 2.0
 set -e
 set -o pipefail
 
