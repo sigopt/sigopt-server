e3d6ae8450772c4d4d1a3063671343d0ff64476c
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-04-11 09:01:33 -0600

    option for mypy out if installed

--- tools/protobuf/compile.sh
+++ tools/protobuf/compile.sh
@@ -5,10 +5,16 @@
 set -e
 set -o pipefail
 
+ARGS=()
+if which protoc-gen-mypy &>/dev/null; then
+  ARGS=( --mypy_out=src/python )
+fi
+
 mkdir -p src/python
 
 find src/protobuf/zigopt/protobuf/gen -name '*.proto' -print0 | xargs -0 \
   protoc \
   -I=src/protobuf \
   -I=/usr/local/include \
-  --python_out=src/python
+  --python_out=src/python \
+  "${ARGS[@]}"
