071312cc8dbb022be64b96b068838ee6616c1b6b
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-04-11 08:44:06 -0600

    dont pin

--- tools/protobuf/compile.sh
+++ tools/protobuf/compile.sh
@@ -5,23 +5,6 @@
 set -e
 set -o pipefail
 
-MIN_VERSION="3.12"
-MAX_VERSION="3.99"
-
-set +e
-CURRENT_VERSION="$(protoc --version | sed 's/libprotoc //')"
-set -e
-
-CLAMPED_MIN_VERSION="$(printf "%s\\n%s\\n" "$MIN_VERSION" "$CURRENT_VERSION" | sort --version-sort | head -n 1)"
-CLAMPED_MAX_VERSION="$(printf "%s\\n%s\\n" "$MAX_VERSION" "$CURRENT_VERSION" | sort --version-sort | tail -n 1)"
-
-if [[ "$CLAMPED_MIN_VERSION" != "$MIN_VERSION" || "$CLAMPED_MAX_VERSION" != "$MAX_VERSION" ]]; then
-  >&2 echo "Expected protoc version between \`v$MIN_VERSION\` and \`v$MAX_VERSION\`, and you have \`v$CURRENT_VERSION\`"
-  >&2 echo
-  >&2 echo "On Mac, run \`brew install protobuf\`"
-  exit 1
-fi
-
 mkdir -p src/python
 
 find src/protobuf/zigopt/protobuf/gen -name '*.proto' -print0 | xargs -0 \
