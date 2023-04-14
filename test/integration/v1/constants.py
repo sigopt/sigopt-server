77e048bd053c3741fc26388d197261c9158ba289
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-04-14 16:11:54 -0600

    types for experiment meta

--- test/integration/v1/constants.py
+++ test/integration/v1/constants.py
@@ -1,10 +1,127 @@
 # Copyright © 2022 Intel Corporation
 #
 # SPDX-License-Identifier: Apache License 2.0
+from __future__ import annotations
+
 from copy import deepcopy
+from typing import Generic, Literal, TypeVar
+
+from typing_extensions import Required, TypedDict
 
 
-DEFAULT_EXPERIMENT_META = dict(
+class MetricMetaType(TypedDict, total=False):
+  name: Required[str]
+  objective: Literal["minimize"] | Literal["maximize"] | None
+
+
+class OptimizeMetricMetaType(MetricMetaType, total=False):
+  threshold: float | None
+  strategy: Literal["optimize"] | None
+
+
+class ConstraintMetricMetaType(MetricMetaType):
+  threshold: float
+  strategy: Literal["constraint"]
+
+
+BoundType = TypeVar("BoundType", bound=int | float)
+
+
+class BoundsMetaType(TypedDict, Generic[BoundType]):
+  min: BoundType
+  max: BoundType
+
+
+class ParameterMetaType(TypedDict, total=False):
+  name: Required[str]
+  conditions: dict[str, str | list[str]] | None
+
+
+class IntegerParameterMetaType(ParameterMetaType):
+  type: Literal["int"]
+
+
+class BoundedIntegerParameterMetaType(IntegerParameterMetaType):
+  bounds: BoundsMetaType[int]
+
+
+class GriddedIntegerParameterMetaType(IntegerParameterMetaType, total=False):
+  grid: list[int]
+
+
+class DoubleParameterMetaType(ParameterMetaType):
+  type: Literal["double"]
+
+
+class BoundedDoubleParameterMetaType(DoubleParameterMetaType):
+  bounds: Required[BoundsMetaType[float]]
+
+
+class GriddedDoubleParameterMetaType(DoubleParameterMetaType, total=False):
+  grid: list[float]
+
+
+class CategoricalValueMetaType(TypedDict):
+  name: str
+
+
+class CategoricalParameterMetaType(ParameterMetaType):
+  type: Literal["categorical"]
+  categorical_values: Required[list[CategoricalValueMetaType | str]]
+
+
+class ConditionalMeta(TypedDict):
+  name: str
+  values: list[str]
+
+
+class LinearConstraintTermMeta(TypedDict):
+  name: str
+  weight: float
+
+
+class LinearConstraintMeta(TypedDict):
+  type: Literal["greater_than"] | Literal["less_than"]
+  terms: list[LinearConstraintTermMeta]
+  threshold: float
+
+
+class TaskMeta(TypedDict):
+  name: str
+  cost: float
+
+
+class BaseExperimentMetaType(TypedDict, total=False):
+  type: str
+  name: str | None
+  parameters: Required[
+    list[
+      BoundedIntegerParameterMetaType
+      | GriddedIntegerParameterMetaType
+      | BoundedDoubleParameterMetaType
+      | GriddedDoubleParameterMetaType
+      | CategoricalParameterMetaType
+    ]
+  ]
+  conditionals: list[ConditionalMeta] | None
+  num_solutions: int | None
+  linear_constraints: list[LinearConstraintMeta]
+
+
+class CoreExperimentMetaType(BaseExperimentMetaType, total=False):
+  metric: OptimizeMetricMetaType | ConstraintMetricMetaType | None
+  metrics: list[OptimizeMetricMetaType | ConstraintMetricMetaType] | None
+  observation_budget: int | None
+  project: str | None
+  tasks: list[TaskMeta]
+
+
+class AiExperimentMetaType(BaseExperimentMetaType):
+  metrics: Required[list[MetricMetaType]]
+  budget: int | None
+
+
+DEFAULT_EXPERIMENT_META: CoreExperimentMetaType = dict(
   name="default experiment",
   metrics=[dict(name="profit")],
   observation_budget=30,
@@ -15,7 +132,7 @@ DEFAULT_EXPERIMENT_META = dict(
   ],
 )
 
-DEFAULT_AI_EXPERIMENT_META = dict(
+DEFAULT_AI_EXPERIMENT_META: AiExperimentMetaType = dict(
   name="default ai experiment",
   metrics=[dict(name="profit")],
   budget=30,
@@ -26,7 +143,7 @@ DEFAULT_AI_EXPERIMENT_META = dict(
   ],
 )
 
-EXPERIMENT_META_CONDITIONALS = dict(
+EXPERIMENT_META_CONDITIONALS: CoreExperimentMetaType = dict(
   name="conditional experiment",
   metrics=[dict(name="metric")],
   observation_budget=30,
@@ -40,7 +157,7 @@ EXPERIMENT_META_CONDITIONALS = dict(
   ],
 )
 
-EXPERIMENT_META_MULTICONDITIONAL = dict(
+EXPERIMENT_META_MULTICONDITIONAL: CoreExperimentMetaType = dict(
   name="multiconditional experiment",
   metrics=[dict(name="metric")],
   observation_budget=30,
@@ -56,7 +173,7 @@ EXPERIMENT_META_MULTICONDITIONAL = dict(
   ],
 )
 
-EXPERIMENT_META_MULTIMETRIC = dict(
+EXPERIMENT_META_MULTIMETRIC: CoreExperimentMetaType = dict(
   name="multimetric experiment",
   metrics=[dict(name="revenue"), dict(name="sales")],
   observation_budget=50,
@@ -67,23 +184,19 @@ EXPERIMENT_META_MULTIMETRIC = dict(
   ],
 )
 
-EXPERIMENT_META_MULTIMETRIC_MINIMIZED = deepcopy(EXPERIMENT_META_MULTIMETRIC)
-EXPERIMENT_META_MULTIMETRIC_MINIMIZED.update(
-  metrics=[
-    dict(name="m1", objective="minimize"),
-    dict(name="m2", objective="minimize"),
-  ],
-)
+EXPERIMENT_META_MULTIMETRIC_MINIMIZED: CoreExperimentMetaType = deepcopy(EXPERIMENT_META_MULTIMETRIC)
+EXPERIMENT_META_MULTIMETRIC_MINIMIZED["metrics"] = [
+  dict(name="m1", objective="minimize"),
+  dict(name="m2", objective="minimize"),
+]
 
-EXPERIMENT_META_MULTIMETRIC_THRESHOLD = deepcopy(EXPERIMENT_META_MULTIMETRIC)
-EXPERIMENT_META_MULTIMETRIC_THRESHOLD.update(
-  metrics=[
-    dict(name="revenue", threshold=None),
-    dict(name="sales", threshold=4.0),
-  ],
-)
+EXPERIMENT_META_MULTIMETRIC_THRESHOLD: CoreExperimentMetaType = deepcopy(EXPERIMENT_META_MULTIMETRIC)
+EXPERIMENT_META_MULTIMETRIC_THRESHOLD["metrics"] = [
+  dict(name="revenue", threshold=None),
+  dict(name="sales", threshold=4.0),
+]
 
-EXPERIMENT_META_MULTISOLUTION = dict(
+EXPERIMENT_META_MULTISOLUTION: CoreExperimentMetaType = dict(
   name="multiopt experiment",
   metrics=[dict(name="profit")],
   observation_budget=30,
@@ -95,7 +208,7 @@ EXPERIMENT_META_MULTISOLUTION = dict(
   num_solutions=2,
 )
 
-EXPERIMENT_META_WITH_CONSTRAINTS = dict(
+EXPERIMENT_META_WITH_CONSTRAINTS: CoreExperimentMetaType = dict(
   name="default constrained experiment",
   metrics=[dict(name="profit")],
   observation_budget=30,
@@ -142,7 +255,7 @@ EXPERIMENT_META_WITH_CONSTRAINTS = dict(
   ],
 )
 
-EXPERIMENT_META_MULTITASK = dict(
+EXPERIMENT_META_MULTITASK: CoreExperimentMetaType = dict(
   name="multitask experiment",
   metrics=[dict(name="profit")],
   observation_budget=30,
@@ -154,7 +267,7 @@ EXPERIMENT_META_MULTITASK = dict(
   tasks=[dict(name="cheapest", cost=0.1), dict(name="cheap", cost=0.5), dict(name="expensive", cost=1)],
 )
 
-EXPERIMENT_META_GRID = deepcopy(DEFAULT_EXPERIMENT_META)
+EXPERIMENT_META_GRID: CoreExperimentMetaType = deepcopy(DEFAULT_EXPERIMENT_META)
 EXPERIMENT_META_GRID["type"] = "grid"
 EXPERIMENT_META_GRID["parameters"] = [
   dict(name="a", type="int", grid=[1, 2, 3, 4, 5]),
@@ -163,7 +276,7 @@ EXPERIMENT_META_GRID["parameters"] = [
 ]
 del EXPERIMENT_META_GRID["observation_budget"]
 
-EXPERIMENT_META_SEARCH = dict(
+EXPERIMENT_META_SEARCH: CoreExperimentMetaType = dict(
   name="search experiment",
   metrics=[
     dict(name="constraint-1", strategy="constraint", threshold=2.1),
@@ -178,7 +291,7 @@ EXPERIMENT_META_SEARCH = dict(
   ],
 )
 
-ALL_META = dict(
+ALL_META: dict[str, CoreExperimentMetaType] = dict(
   conditionals=EXPERIMENT_META_CONDITIONALS,
   constraints=EXPERIMENT_META_WITH_CONSTRAINTS,
   default=DEFAULT_EXPERIMENT_META,
