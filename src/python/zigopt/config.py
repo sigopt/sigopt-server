21d97842fa4eaa9f5f10c6f8467f071e5fbeca71
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-04-19 14:58:40 -0600

    dump environ

--- src/python/zigopt/config.py
+++ src/python/zigopt/config.py
@@ -12,4 +12,5 @@ DEFAULT_CONFIG_DIR = "/etc/sigopt/server-config/"
 def load_config_from_env() -> ConfigBroker:
   config_dir = os.environ.get("SIGOPT_SERVER_CONFIG_DIR") or DEFAULT_CONFIG_DIR
   print(os.environ.get("SIGOPT_SERVER_CONFIG_DIR"), config_dir)
+  print(os.environ)
   return ConfigBroker.from_directory(config_dir)
