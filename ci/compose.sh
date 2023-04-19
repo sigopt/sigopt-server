74ab964db1293481f9c5517cad1aa7c99892029e
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-04-19 13:38:52 -0600

    fixes

--- ci/compose.sh
+++ ci/compose.sh
@@ -7,7 +7,6 @@ set -o pipefail
 
 export COMPOSE_PROJECT_NAME="${CIRCLE_SHA1}_${CIRCLE_JOB}_${CIRCLE_NODE_INDEX}"
 export TAG="$CIRCLE_SHA1"
-export SIGOPT_SERVER_CONFIG_DIR=/sigopt-server/config/circleci/
 export MINIO_ROOT_PASSWORD=CHANGEME123
 
 exec docker-compose --file=docker-compose.yml --env-file=./.env "$@"
