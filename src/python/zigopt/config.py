4eff84281ef7c15fd44aeef45be190c78559c26c
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-04-19 15:14:25 -0600

    remove the debugging

--- src/python/zigopt/config.py
+++ src/python/zigopt/config.py
@@ -11,6 +11,4 @@ DEFAULT_CONFIG_DIR = "/etc/sigopt/server-config/"
 
 def load_config_from_env() -> ConfigBroker:
   config_dir = os.environ.get("SIGOPT_SERVER_CONFIG_DIR") or DEFAULT_CONFIG_DIR
-  print(os.environ.get("SIGOPT_SERVER_CONFIG_DIR"), config_dir)
-  print(os.environ)
   return ConfigBroker.from_directory(config_dir)
