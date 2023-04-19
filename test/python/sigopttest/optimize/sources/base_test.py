e9d39c98d2eed4a56a84d7bb7812eb276fad99fc
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-04-19 11:32:43 -0600

    on data

--- test/python/sigopttest/optimize/sources/base_test.py
+++ test/python/sigopttest/optimize/sources/base_test.py
@@ -238,7 +238,7 @@ class TestSourcesGetSuggestionsLogScale(UnitTestBase):
     )
 
   def test_categorical_suggestions_with_parameter_log_transform(self, services, experiment):
-    model = services.config_broker.setdefault("model", {})
+    model = services.config_broker.data.setdefault("model", {})
     model.update(
       {
         "max_simultaneous_af_points": 10,
@@ -266,7 +266,7 @@ class TestSourcesGetSuggestionsLogScale(UnitTestBase):
 
   @pytest.mark.parametrize("num_observations", [3, 20, 50, 90])
   def test_spe_suggestions_with_parameter_log_transform(self, services, experiment, num_observations):
-    services.config_broker.setdefault("model", {})["num_suggestions"] = 2
+    services.config_broker.data.setdefault("model", {})["num_suggestions"] = 2
 
     source = SPEOptimizationSource(services, experiment)
     services.sc_adapter = SCAdapter(services)
