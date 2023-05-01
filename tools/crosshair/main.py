69be722b072562621ef33b68db0dfee06ad61f6e
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-05-01 14:39:48 -0600

    default runner for crosshair disables crosshair

---
+++ tools/crosshair/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright © 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache License 2.0
+import crosshair.main
+
+
+crosshair.main.DEFAULT_OPTIONS.enabled = False
+
+if __name__ == "__main__":
+  crosshair.main.main()
