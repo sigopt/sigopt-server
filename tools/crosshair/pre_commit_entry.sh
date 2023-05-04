df582c2d12fc3cfaad62374b9aa6d09e276859d9
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-05-04 10:45:39 -0600

    output all at once, add parallelism

--- tools/crosshair/pre_commit_entry.sh
+++ tools/crosshair/pre_commit_entry.sh
@@ -6,20 +6,21 @@ set -e
 set -o pipefail
 
 if [ -z "$_RUN_CROSSHAIR" ]; then
+  OUTPUT="$(mktemp)"
   if crosshair check \
     --per_path_timeout=0.01 \
     --per_condition_timeout=2 \
     --analysis_kind=deal \
-    "$@"
+    "$@" &>"$OUTPUT"
   then
     exit
-  else
-    echo "The above failure occurred while checking $1"
-    echo
-    exit 1
   fi
+  echo "The above failure occurred while checking $1" >>"$OUTPUT"
+  echo >>"$OUTPUT"
+  cat "$OUTPUT"
+  exit 1
 fi
 
 
 source scripts/set_python_path.sh .
-printf "%s\0" "$@" | xargs -0 -n1 env _RUN_CROSSHAIR=x "$0"
+printf "%s\0" "$@" | xargs -0 -n1 -P4 env _RUN_CROSSHAIR=x "$0"
