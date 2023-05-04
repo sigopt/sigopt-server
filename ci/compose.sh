4c2964276747df78360797784d2932d72fda89a8
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-05-04 15:37:55 -0600

    relative to root

--- ci/compose.sh
+++ ci/compose.sh
@@ -7,6 +7,6 @@ set -o pipefail
 
 export TAG="$CIRCLE_SHA1"
 export MINIO_ROOT_PASSWORD=CHANGEME123
-export SIGOPT_SERVER_CONFIG_DIR=config/circleci/
+export SIGOPT_SERVER_CONFIG_DIR=/sigopt-server/config/circleci/
 
 exec docker compose --file=docker-compose.yml --env-file=./.env "$@"
