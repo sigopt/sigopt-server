fb095e7c1bef2d2f512f43dd68a23790c1df61d9
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-05-31 12:50:18 -0600

    more build memory?

--- scripts/compile/minify_js_server_bundle.sh
+++ scripts/compile/minify_js_server_bundle.sh
@@ -6,7 +6,7 @@ set -e
 set -o pipefail
 
 export NODE_DOMAIN=server
-export NODE_OPTIONS="--max-old-space-size=2048"
+export NODE_OPTIONS="--max-old-space-size=4096"
 exec yarn -s webpack \
   --config=web/js/webpack/server_side.config.babel.js \
   --env=NODE_ENV=production
