f1c410a530145219dd5ba4ce52e0f166eac326d6
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-06-15 10:55:48 -0600

    note service typing

--- src/python/zigopt/note/service.py
+++ src/python/zigopt/note/service.py
@@ -1,7 +1,14 @@
 # Copyright © 2022 Intel Corporation
 #
 # SPDX-License-Identifier: Apache License 2.0
+from typing import Any, Mapping
+
+from sqlalchemy.orm import Query
+
+from zigopt.client.model import Client
+from zigopt.db.declarative import Base
 from zigopt.note.model import ProjectNote
+from zigopt.project.model import Project
 from zigopt.services.base import Service
 
 
@@ -13,15 +20,15 @@ class NoteService(Service):
     ],
   }
 
-  def _build_base_read_query(self, NoteCls, resource_id_map):
+  def _build_base_read_query(self, NoteCls: type[Base], resource_id_map: Mapping[str, Any]) -> Query:
     query = self.services.database_service.query(NoteCls)
     for parent_resource, fkey in self.fkeys[NoteCls]:
       query = query.filter(fkey == resource_id_map[parent_resource])
     return query
 
-  def find_project_notes_by_client_and_project(self, client, project):
+  def find_project_notes_by_client_and_project(self, client: Client, project: Project) -> Query:
     resource_id_map = {"client": client.id, "project": project.id}
     return self._build_base_read_query(ProjectNote, resource_id_map)
 
-  def insert(self, note):
+  def insert(self, note: Base) -> None:
     self.services.database_service.insert(note)
