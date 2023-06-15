5d92d12f85852222862654ba40bee576ac20e5b5
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-06-15 10:49:28 -0600

    typing message tracking service

--- src/python/zigopt/message_tracking/service.py
+++ src/python/zigopt/message_tracking/service.py
@@ -3,30 +3,31 @@
 # SPDX-License-Identifier: Apache License 2.0
 import contextlib
 import uuid
+from collections.abc import Generator
 
 from zigopt.common.sigopt_datetime import unix_timestamp
 from zigopt.services.base import Service
 
 
 class BaseMessageTrackingService(Service):
-  def count_processing_messages(self, queue_name):
+  def count_processing_messages(self, queue_name: str) -> int:
     raise NotImplementedError()
 
   @contextlib.contextmanager
-  def process_one_message(self, queue_name):
+  def process_one_message(self, queue_name: str) -> Generator:
     raise NotImplementedError()
 
 
 class MessageTrackingService(BaseMessageTrackingService):
-  def _get_redis_key(self, queue_name):
+  def _get_redis_key(self, queue_name: str) -> str:
     return self.services.redis_key_service.create_queue_tracking_key(queue_name)
 
-  def count_processing_messages(self, queue_name):
+  def count_processing_messages(self, queue_name: str) -> int:
     key = self._get_redis_key(queue_name)
     return self.services.redis_service.count_sorted_set(key)
 
   @contextlib.contextmanager
-  def process_one_message(self, queue_name):
+  def process_one_message(self, queue_name: str) -> Generator:
     key = self._get_redis_key(queue_name)
     set_member = str(uuid.uuid1())
     now = unix_timestamp()
