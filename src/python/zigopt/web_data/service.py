0fd2bef8eb58cef905f7e4a67b29cfb4fcac235e
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-06-16 15:09:02 -0600

    web data typing

--- src/python/zigopt/web_data/service.py
+++ src/python/zigopt/web_data/service.py
@@ -1,14 +1,18 @@
 # Copyright © 2022 Intel Corporation
 #
 # SPDX-License-Identifier: Apache License 2.0
-from sqlalchemy import desc
+from collections.abc import Mapping, Sequence
 
+from sqlalchemy import desc
+from sqlalchemy.orm import Query
+
+from zigopt.db.service import DatabaseService
 from zigopt.services.base import Service
 from zigopt.web_data.lib import validate_web_data_dict
 from zigopt.web_data.model import WebData
 
 
-def project_query(db_service, parent_resource_id):
+def project_query(db_service: DatabaseService, parent_resource_id: Mapping[str, int]) -> Query:
   return (
     db_service.query(WebData)
     .filter(WebData.project_reference_id == parent_resource_id["project"])
@@ -21,7 +25,13 @@ validate_web_data_dict(queries_by_resource, depth=1)
 
 
 class WebDataService(Service):
-  def count(self, parent_resource_type, web_data_type, parent_resource_id, include_deleted=False):
+  def count(
+    self,
+    parent_resource_type: str,
+    web_data_type: str,
+    parent_resource_id: Mapping[str, int],
+    include_deleted: bool = False,
+  ) -> int:
     query = (
       queries_by_resource[parent_resource_type](self.services.database_service, parent_resource_id)
       .filter(WebData.deleted == include_deleted)
@@ -29,7 +39,13 @@ class WebDataService(Service):
     )
     return self.services.database_service.count(query)
 
-  def all(self, parent_resource_type, web_data_type, parent_resource_id, include_deleted=False):
+  def all(
+    self,
+    parent_resource_type: str,
+    web_data_type: str,
+    parent_resource_id: Mapping[str, int],
+    include_deleted: bool = False,
+  ) -> Sequence[WebData]:
     query = (
       queries_by_resource[parent_resource_type](self.services.database_service, parent_resource_id)
       .filter(WebData.deleted == include_deleted)
@@ -40,12 +56,12 @@ class WebDataService(Service):
 
   def find_by_parent_resource_id_and_id_query(
     self,
-    parent_resource_type,
-    web_data_type,
-    parent_resource_id,
-    base_id,
-    include_deleted=False,
-  ):
+    parent_resource_type: str,
+    web_data_type: str,
+    parent_resource_id: Mapping[str, int],
+    base_id: int,
+    include_deleted: bool = False,
+  ) -> Query:
     query = (
       queries_by_resource[parent_resource_type](self.services.database_service, parent_resource_id)
       .filter(WebData.id == base_id)
@@ -56,12 +72,12 @@ class WebDataService(Service):
 
   def find_by_parent_resource_id_and_id(
     self,
-    parent_resource_type,
-    web_data_type,
-    parent_resource_id,
-    base_id,
-    include_deleted=False,
-  ):
+    parent_resource_type: str,
+    web_data_type: str,
+    parent_resource_id: Mapping[str, int],
+    base_id: int,
+    include_deleted: bool = False,
+  ) -> WebData | None:
     query = self.find_by_parent_resource_id_and_id_query(
       parent_resource_type,
       web_data_type,
@@ -71,6 +87,6 @@ class WebDataService(Service):
     )
     return self.services.database_service.one_or_none(query)
 
-  def delete(self, base_id):
+  def delete(self, base_id: int) -> int:
     query = self.services.database_service.query(WebData).filter(WebData.id == base_id)
     return self.services.database_service.update_one(query, {"deleted": True})
