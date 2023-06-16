a6337200656ef15bf7c17a217c5df6fd4c8f4e2a
Taylor Jackle Spriggs <taylor.spriggs@intel.com>
2023-06-16 15:16:35 -0600

    fix disabled service

--- src/python/zigopt/services/disabled.py
+++ src/python/zigopt/services/disabled.py
@@ -1,10 +1,10 @@
 # Copyright © 2022 Intel Corporation
 #
 # SPDX-License-Identifier: Apache License 2.0
-from zigopt.services.base import GlobalService
+from zigopt.services.base import BaseService
 
 
-class DisabledService(GlobalService):
+class DisabledService(BaseService):
   @property
   def enabled(self):
     return False
